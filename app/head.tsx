0cf892c5f471cbef5c645d032f4d2fc07ba32ec3
Muzammil Makandar <105341753+muzammil2406@users.noreply.github.com>
2025-06-22 16:36:15 +0530

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -8,13 +8,13 @@ export default function Head() {
       <meta property="og:title" content="Portfolio | Muzammil Makandar - Full Stack Developer" />
       <meta property="og:description"
         content="I'm Muzammil Makandar, full-stack developer based in Belgaum, India. My journey in web development began during my first year of college." />
-      <meta property="og:url" content="https://muzammil-dev.netlify.app" />
-      <meta property="og:image" content="https://muzammil-dev.netlify.app/portfolio-fork.png" />
-      <link rel="canonical" href="https://muzammil-dev.netlify.app" />
+      <meta property="og:url" content="https://muzammil-dev.vercel.app" />
+      <meta property="og:image" content="https://muzammil-dev.vercel.app/portfolio-fork.png" />
+      <link rel="canonical" href="https://muzammil-dev.vercel.app" />
       <meta name="author" content="Muzammil Makandar" />
       <meta name="language" content="English" />
       <meta name="twitter:card" content="summary" />
-      <meta name="twitter:site" content="https://muzammil-dev.netlify.app" />
+      <meta name="twitter:site" content="https://muzammil-dev.vercel.app" />
       <meta name="application-name" content="Portfolio | Muzammil Makandar - Full Stack Developer" />
       <meta name="apple-mobile-web-app-title" content="Portfolio | Muzammil Makandar - Full Stack Developer" />
       <link rel="icon" href="/favicon.ico" />
