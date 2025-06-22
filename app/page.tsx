e1f28011d096ce1010f687bc56f3ff8ed3fff04a
Muzammil Makandar <105341753+muzammil2406@users.noreply.github.com>
2025-06-22 16:41:36 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default async function page() {
   return (
     <>
       {data ?
-        <HomePage data={data} />
+        <HomePage />
         :
         <div className='h-screen w-screen flex flex-col items-center justify-center gap-5 text-violet-600 fixed z-30 bg-gray-100 dark:bg-grey-900'>
           <FaNodeJs size={100} className='animate-pulse' />
